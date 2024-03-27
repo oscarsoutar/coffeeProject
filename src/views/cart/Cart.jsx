82cd4cf76f749c0f14b35ed5324d0b0b0b01b140
oscarsoutar <oscarchanadol@gmail.com>
2024-03-28 01:50:36 +0700

    clean

--- src/views/cart/Cart.jsx
+++ src/views/cart/Cart.jsx
@@ -35,88 +35,6 @@ export default function viewCart() {
     };
 
 
-    // const confirmOrder = async () => {
-    //     if (userProfile) {
-    //         const orderData = {
-    //             menus: cart.menus,
-    //             toppings: cart.toppings,
-    //             quantity: cart.quantity,
-    //             comment: cart.comment
-    //         };
-
-    //         await axios.post('https://bubble-tea-cafe-api-production.up.railway.app/api/order', {
-    //             headers: {
-    //                 Authorization: token
-    //             }
-    //         });
-
-    //         userProfile.setCart({});
-    //     }
-
-    // const confirmOrder = async () => {
-    //     if (userProfile) {
-    //         const orderData = {
-    //             user_id: userProfile.id,
-    //             menus: cart.menus,
-    //             toppings: cart.toppings,
-    //             quantity: cart.quantity,
-    //             comment: cart.comment,
-    //             total: calculateTotalCost(cart.menus, cart.toppings, cart.quantity),
-    //             status: "pending"
-    //         };
-    
-    //         try {
-    //             const response = await axios.post('https://bubble-tea-cafe-api-production.up.railway.app/api/order', orderData, {
-    //                 headers: {
-    //                     Authorization: token
-    //                 }
-    //             });
-    
-    //             if (response.status === 201) {
-    //                 userProfile.setCart({});
-    //             }
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     }
-    // }
-
-    // async function calculateTotalCost(menus, toppings, quantity) {
-    //     let totalCost = 0;
-    
-    //     // Calculate the cost of the menus
-    //     for (const menu of menus) {
-    //         // Get the price of the menu from the API
-    //         const menuResponse = await axios.get(`https://bubble-tea-cafe-api-production.up.railway.app/api/menu/${menu.Id}`, {
-    //             headers: {
-    //                 Authorization: token
-    //             }
-    //         });
-    //         const menuPrice = menuResponse.data.data.price;
-    
-    //         // Add the cost of the menu to the total cost
-    //         totalCost += menuPrice * quantity;
-    //     }
-    
-    //     // Calculate the cost of the toppings
-    //     for (const topping of toppings) {
-    //         // Get the price of the topping from the API
-    //         const toppingResponse = await axios.get(`https://bubble-tea-cafe-api-production.up.railway.app/api/topping/${topping}`, {
-    //             headers: {
-    //                 Authorization: token
-    //             }
-    //         });
-    //         const toppingPrice = toppingResponse.data.data.price;
-    
-    //         // Add the cost of the topping to the total cost
-    //         totalCost += toppingPrice;
-    //     }
-    
-    //     return totalCost;
-    // }
-
-
-
     const clearCart = async (e) => {
         e.preventDefault();
         await axios.delete('https://bubble-tea-cafe-api-production.up.railway.app/api/auth/remove-all-from-cart', {
@@ -126,61 +44,6 @@ export default function viewCart() {
         })
         window.location.reload();
     }
-
-
-
-    
-    // const [selectedItem, setSelectedItem] = useState([]);
-    // const handleItemClick = (item) => {
-    //     // e.preventDefault();
-    //     // setSelectedItem(e);
-    //     setSelectedItem(item);
-    //     // (e) => removeItem(e)
-    // }
-    // console.log(selectedItem);
-    // console.log(selectedItem.Id);
-
-    // const removeItem = async (e) => {
-    //     e.preventDefault();
-    //     var itemId
-
-    //     cart.map((item) => { 
-    //         if(item.id === selectedItem.id) {
-    //             itemId= item.id
-    //         }
-    //     })
-
-    //     await axios.delete('https://bubble-tea-cafe-api-production.up.railway.app/api/auth/remove-from-cart/' + selectedItem.id //find cartId
-    //     , {
-    //         headers: {
-    //             Authorization: token
-    //         }
-    //     })
-    //     window.location.reload();
-    // }
-
-
-
-
-    // const menuDelete = async (e) => {
-    //     e.preventDefault();
-
-    //     let menuSelect = document.querySelector('#menuSelect').value;
-    //     var menuId
-
-    //     menus.map((menu) => {
-    //         if (menu.name == menuSelect) {
-    //             menuId = menu.Id
-    //         }
-    //     })
-    //     await axios.delete('https://bubble-tea-cafe-api-production.up.railway.app/api/menu/' + menuId, {
-    //         headers: {
-    //             Authorization: token
-    //         }
-    //     })
-    //     window.location.reload();
-    // }
-
 
 
     function toppingnamegrab(order) {
@@ -236,10 +99,6 @@ export default function viewCart() {
                         <td>{item.status}</td>
                         <td>
                             <button>confirm order</button>
-                            {/* <button onClick={handleItemClick(item)}>remove</button> */}
-                            {/* <button onClick={(e) => { handleItemClick(item); removeItem(e) }}>remove</button> */}
-                            {/* {() => { setSelectedItem(item); handleRemoveItem() }} */}
-                            {/* <button onClick={(e) => removeItem(e)}>remove</button> */}
                             <button>remove</button> 
                             <button>edit</button>
                         </td>
